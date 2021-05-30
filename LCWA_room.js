7aaf2a9ee8fd1f0c29907d1e908f4436ae937d07
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2021-05-30 16:12:35 +0530

    Add files via upload

---
+++ LCWA_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAd0u3QvhCnSchmqIJEEdc41JOMVDNEbjg",
+    authDomain: "project47-6aa8b.firebaseapp.com",
+    databaseURL: "https://project47-6aa8b.firebaseio.com",
+    projectId: "project47-6aa8b",
+    storageBucket: "project47-6aa8b.appspot.com",
+    messagingSenderId: "889037430714",
+    appId: "1:889037430714:web:ed326bb964da385fb48783"
+  };
+  // Initialize Firebase
